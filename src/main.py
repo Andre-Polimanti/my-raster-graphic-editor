1a3fdae3119a9d23efd173ed23d8d7025c8885df
linguissa <andrepolimanti.ap@gmail.com>
2026-05-20 13:27:14 -0300

    Bresenham implementation and testing

--- src/main.py
+++ src/main.py
@@ -1,18 +1,30 @@
 from OpenGL.GL import *
 
 from gui.MainWindow import MainWindow
-from lib.primitives.lines.dda import draw_line
+from lib.primitives.lines.bresenham import draw_line
 
 def main():
     w = 800
     h = 600
     app = MainWindow(w,h)
 
-    draw_line(app.canvas.backbuffer, 0,h//2, w-1,h//2, (0,0,0,255))
+    max_w = w - 1
+    max_h = h - 1
+
+
+    draw_line(app.canvas.backbuffer, max_w,max_h, 0,0, (0,0,0,255))
+    draw_line(app.canvas.backbuffer, 0,max_h, max_w,0, (0,0,0,255))
     app.canvas.upload_backbuffer()
-    draw_line(app.canvas.backbuffer, w//2,0, w//2,h-1,(0,0,0,255))
+
+
+    draw_line(app.canvas.backbuffer, max_w//2,max_h, max_w//2,0, (0,0,0,255))
+    draw_line(app.canvas.backbuffer, 0,max_h//2, max_w,max_h//2, (0,0,0,255))
+    app.canvas.upload_backbuffer()
+
+
+    draw_line(app.canvas.backbuffer, max_w,max_h, 0, max_h//2, (0,0,0,255))
+    draw_line(app.canvas.backbuffer, 0,max_h, max_w, max_h//2, (0,0,0,255))
     app.canvas.upload_backbuffer()
 
     app.run()
-
 main()
