b6a260c3b3c70c8fb6032f16f19689390e01aec3
linguissa <andrepolimanti.ap@gmail.com>
2026-06-08 21:34:44 -0300

    Commenting on rectangle drawing

--- src/lib/others/shapes/rectangle/bresenham.py
+++ src/lib/others/shapes/rectangle/bresenham.py
@@ -1,6 +1,7 @@
 from ....primitives.lines.bresenham import draw_line
 
 def draw_rectangle(buffer, x0:int,y0:int, x1:int,y1:int, color:tuple, size:int):
+    # Draw lines from extremity to extremity
     draw_line(buffer, x0,y0, x0,y1, color,size)
     draw_line(buffer, x0,y0, x1,y0, color,size)
 
