2582fb34db685db214e95145bf8ea1220c9c4de1
linguissa <andrepolimanti.ap@gmail.com>
2026-06-08 21:05:10 -0300

    Adding comments on memory handling classes

--- src/core/back_buffer.py
+++ src/core/back_buffer.py
@@ -1,11 +1,13 @@
 from .front_buffer import FrontBuffer
 
+# This buffer is to be used as a temporary layer, where only the current edit is stored, once it's comitted or aborted, this buffer is cleared.
+# The pixels that are touched by the current edit will be put above the FrontBuffer, so that the user can see a preview of his work.
 class BackBuffer(FrontBuffer):
     def __init__(self, front_buffer:FrontBuffer):    
-        super().__init__(front_buffer.w,front_buffer.h, (0,0,0,0))
-
+        super().__init__(front_buffer.w,front_buffer.h, (0,0,0,0)) #It's background color is transparent.
         self.front_buffer = front_buffer
-        self.dirty_pixels = set()
+
+        self.dirty_pixels = set() # Pixels touched by the edit.
 
     def put_pixel(self, x:int, y:int, color:tuple):
         if super().put_pixel(x,y, color):
@@ -17,11 +19,13 @@ class BackBuffer(FrontBuffer):
     def clear(self):
         for x, y in self.dirty_pixels:
             super().put_pixel(x, y, self.bg_color)
+
         self.dirty_pixels.clear()
         #print("BackBuffer cleared!")
     
     def commit(self):
         for x,y in self.get_dirty_pixels():
             color = self.get_pixel(x,y)
-            self.front_buffer.put_pixel(x,y,color)
+            self.front_buffer.put_pixel(x,y,color) # The pixels touched by the edit are painted on the FrontBuffer
+            
         self.clear()
--- src/core/front_buffer.py
+++ src/core/front_buffer.py
@@ -5,15 +5,19 @@ class FrontBuffer:
         self.bg_color = background_color
 
         r,g,b,a= background_color
-        self.pixels = bytearray([r, g, b, a] * (width * height))
+        self.pixels = bytearray([r, g, b, a] * (width * height)) # glDrawPixel, the function used for actual painting, only works with 1D arrays, so we have to make sure this is the format we are working with
 
     def is_valid_pixel(self, x:int,y:int):
         return (0 <= x < self.w and 0 <= y < self.h)
+    
 
+    # Since we are working with a 1D array, we have to adapt our memory accessing methods.
+    # (y * self.w) -> jumps the image lines that are anterior to the pixel we want to access
+    # ((y * self.w) + x) -> on the line of the wanted pixel, we go to its column
+    # ((y * self.w) + x) * 4 -> our pixels have four bytes of colour, so we have to multiply it.
     def put_pixel(self, x:int,y:int, color:tuple):
-        is_valid_operation = self.is_valid_pixel(x,y)
-        if is_valid_operation:
-            idx = (y * self.w + x) * 4
+        if self.is_valid_pixel(x,y):
+            idx = ((y * self.w) + x) * 4
             self.pixels[idx]     = color[0] # R
             self.pixels[idx + 1] = color[1] # G
             self.pixels[idx + 2] = color[2] # B
@@ -24,14 +28,15 @@ class FrontBuffer:
             return False
 
     def get_pixel(self, x:int,y:int):
-        is_valid_operation = self.is_valid_pixel(x,y)
-        if is_valid_operation:
-            idx = (y * self.w + x) * 4
+        if self.is_valid_pixel(x,y):
+            idx = ((y * self.w) + x) * 4
             return (self.pixels[idx], self.pixels[idx+1], self.pixels[idx+2], self.pixels[idx+3])
+        
         #print("Invalid pixel!")
         return self.bg_color
     
     def clear(self):
         r, g, b, a= self.bg_color
         self.pixels[:] = bytearray([r, g, b, a] * (self.w * self.h))
+
         #print("FrontBuffer cleared!")
